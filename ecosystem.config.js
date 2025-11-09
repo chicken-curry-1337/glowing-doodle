143b3e40df41e268a48b410b277e2d348b295945
Глеб Белкин <ga_belkin@betboom.org>
2025-11-09 10:15:45 +0300

    feat: ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -11,10 +11,19 @@ const {
 module.exports = {
   apps: [
     {
-      name: 'glowing-backend',
+      name: 'mesto-backend',
       cwd: './backend',
       script: 'npm',
       args: 'run start',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+    {
+      name: 'mesto-frontend',
+      cwd: './frontend',
+      script: 'npm',
+      args: 'run build',
       env: {
         NODE_ENV: 'production',
       },
@@ -30,16 +39,22 @@ module.exports = {
       path: DEPLOY_PATH,
 
       // Кидаем .env на сервер
-      'pre-deploy-local': `scp ./.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend/.env`,
+      'pre-deploy-local': [
+        `scp ./frontend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/frontend/.env`
+        `scp ./backend/.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/backend/.env`
+      ],
 
       // Готовим всё уже на сервере
-      'post-deploy': [
+      'pre-deploy': [
+        // frontend зависимости
+        `cd ${DEPLOY_PATH}/frontend && npm ci`,
         // backend зависимостИ
         `cd ${DEPLOY_PATH}/backend && npm ci`,
 
         // перезапуск бэка
         `pm2 restart ecosystem.config.js --only glowing-backend --env production`
       ].join(' && '),
+      'pre-deploy': `pm2 restart ecosystem.config.js --only mesto-backend --env production`,
     },
 },
 };
